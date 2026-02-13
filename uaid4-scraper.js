803bd53b9e2f1af3b8d0e3bf555b5dadf40035d9
froosterton <jeksonater@gmail.com>
2026-02-12 21:51:49 -0500

    Update uaid4-scraper.js

--- uaid4-scraper.js
+++ uaid4-scraper.js
@@ -5,9 +5,9 @@ const express = require('express');
 const { Client } = require('discord.js-selfbot-v13');
 
 // Configuration - Environment variables for Railway
-const WEBHOOK_URL = process.env.WEBHOOK_URL || 'https://discord.com/api/webhooks/1470215101385805949/EmFY3B5AfxkIC5x_8clEDYsJyJqy59u7VjtQyBNB5OhN66yvo6ljQyItQxxkg8o_qrxp';
+const WEBHOOK_URL = process.env.WEBHOOK_URL || 'https://discord.com/api/webhooks/1424544927215259774/CIwNwKw8SSM2LIxubgMoZjGioui_3Qmoz6h9VGSTqvZL_1eRcQ-hFmaQc_KuvabCToIo';
 const USERNAME_WEBHOOK_URL = process.env.USERNAME_WEBHOOK_URL || 'https://discord.com/api/webhooks/1465281642913599563/37S2bQWecCwslpK6tZXcPc9GunYvrnFY21BMcW8Llh0fbUyquhlN3TWx4Y8vv5K7a3ym';
-const ITEM_IDS = process.env.ITEM_IDS || '98346834,1180433861';
+const ITEM_IDS = process.env.ITEM_IDS || '136802867,188888052,102617707,17735316,16598513,74891470';
 const NEXUS_ADMIN_KEY = process.env.NEXUS_ADMIN_KEY;
 const NEXUS_API_URL = 'https://discord.nexusdevtools.com/lookup/roblox';
 
